721ffd5b01fcdc048bf1713d53b95036d2e1f769
Vivian <vn.gomez@uniandes.edu.co>
2020-09-09 11:22:11 -0500

    Init

--- utils/segmentation.py
+++ utils/segmentation.py
@@ -1,1 +1,8 @@
-#acá implementaremos el algoritmo
+from skimage import io
+import numpy
+
+img = "images/1.jpg"
+imagenCargada = io.imread(img)
+img_width, img_height, depth = imagenCargada.shape
+
+print(img_width, img_height, depth)
